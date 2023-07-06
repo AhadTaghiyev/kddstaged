cf8610664360da05edd9a0e6b3f0d0f4d8e7efc6
AhadTaghiyev <taghiyev.ahad@gmail.com>
2023-07-06 15:47:05 +0400

    done

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿
+$(document).ready(function () {
+    $(function () {
+        $('form').on('keyup keypress', function (e) {
+            var keyCode = e.keyCode || e.which;
+            if (keyCode === 13) {
+                e.preventDefault();
+                return false;
+            }
+        });
+
+
+    });
+});
